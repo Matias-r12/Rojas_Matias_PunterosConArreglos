e0022af1ae580eb8e0445ad3fc29dbdd0e44f554
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-05-13 14:42:28 -0500

    Segunda version del segundo ejercicio

--- PunterosConArreglos2.c
+++ PunterosConArreglos2.c
@@ -2,9 +2,12 @@
 int main(int argc, char const *argv[])
 {
     char vocales[5]={'a','e','i','o','u'};
+    char *ptr;
+    ptr=&vocales[0];
     for (char i = 0; i < 5; i++)
     {
-        printf("vocales[%d] %c\n",i,vocales[i]);
+        printf("vocales[%d] %c ",i,vocales[i]);
+        printf("%c\n",*(ptr++));
     }
     
     return 0;
